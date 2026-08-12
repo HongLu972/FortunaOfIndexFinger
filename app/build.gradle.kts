4fa3bb468737398413081f9610f29184c5591544
玉 <154282423+leochen972@users.noreply.github.com>
2026-08-13 04:37:51 +0800

    v0.5.1
    小小地修了一下，使得音量改的比较方便

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.six.fortuna"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.5"
+        versionCode = 7
+        versionName = "0.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
